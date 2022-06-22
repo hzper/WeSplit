53fd0cd99f2998a9a7a76b2813fdd90927a52a8f
hzper <hzpper@gmail.com>
2022-06-22 10:49:12 +0200

    Decimal pad, Locale.currencyCode, nil coalescing

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,24 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let students = ["Harry","Hermione","Ron"]
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
     
-    @State private var selectedStudent = "Harry"
+    let tipPercentages = [10,15,20,25,0]
     
     var body: some View {
-        NavigationView{
-            Form{
-                Picker("Odaberi svog učenika", selection: $selectedStudent){
-                    ForEach(students,id: \.self){
-                        Text($0)
-                    }
-                }
+        Form{
+            Section{
+                TextField("Amount", value: $checkAmount,format:
+                        .currency(code: Locale.current.currencyCode ?? "USD" ))
+                        .keyboardType(.decimalPad)
             }
-            
+            Section{
+                Text(checkAmount,format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
         }
         
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
